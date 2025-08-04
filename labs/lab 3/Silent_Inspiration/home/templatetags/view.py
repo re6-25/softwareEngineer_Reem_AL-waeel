efcec2256d5eb83d2c0ddbf94cdd0b46d5354525
Reem <reemalwaeel40@gmail.com>
2025-08-05 02:26:54 +0300

    تنفيذ المحاضرة 3+مع التعديل

--- labs/lab 3/Silent_Inspiration/home/templatetags/view.py
+++ labs/lab 3/Silent_Inspiration/home/templatetags/view.py
@@ -5,7 +5,6 @@ def home_page(request):
     converted_text = ''
     if request.method == 'POST':
         input_text = request.POST.get('text', '')
-       # converted_text = yemen_word(input_text)
     return render(request, 'index.html', {
         'username': 'Remi',
         'input_text': input_text,
